75fd5450017d06bf1c8d52f740991eaa294e5545
Anish Ghimire <150002261+TechExplorerAnish@users.noreply.github.com>
2023-12-24 03:36:16 +0000

    self exploring

--- javaScript/java_script_basic/test.js
+++ javaScript/java_script_basic/test.js
@@ -1,1 +1,17 @@
-console.log("test")
+var name = "Anish"
+let lastName = "Ghimire"
+const age = 2
+const userId = null
+let userEmail;
+
+//console.log(undefined+null) //=>NaN and typeof(NaN)=>Number;
+//console.log(typeof(Number(lastName)))//=>Number => Number(lastName)=> NaN;
+//console.log(typeof(String(age))) //=> string ==> String(age)=> "2";
+// console.log(typeof(null)) //=>object;
+// console.log(typeof(undefined))//=>undefined;
+// console.log(2+"3") //=>23;
+// console.log("2"+3) //=>23;
+// console.log("2"+3+4) //=> 234;
+// console.log(2+3+"4") //=>54
+
+
